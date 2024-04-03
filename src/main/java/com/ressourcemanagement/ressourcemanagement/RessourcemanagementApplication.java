9b1f551ed6b421273a0ab6c7fbcab76a767cd84d
darkseid <hamza.chaqchaq@usmba.ac.ma>
2024-04-03 10:23:53 +0000

    Initial Commit

---
+++ src/main/java/com/ressourcemanagement/ressourcemanagement/RessourcemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.ressourcemanagement.ressourcemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RessourcemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RessourcemanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ressourcemanagement/ressourcemanagement/RessourcemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ressourcemanagement.ressourcemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RessourcemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
